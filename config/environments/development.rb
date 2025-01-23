b05fb1577d96ac45aedaf98e17a16a6a982a688c
Caio Mendes <38328503+CaioSMendes@users.noreply.github.com>
2025-01-23 02:04:15 -0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,8 @@ Rails.application.configure do
 
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
-
+  
+  config.hosts << "smartlocker.brasiliarfid.com.br"
   # Do not eager load code on boot.
   config.eager_load = false
 
